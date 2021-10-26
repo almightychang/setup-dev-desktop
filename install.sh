a576328d12f06e2962bd36899d0fe122158e56e5
almightychang <65899329+almightychang@users.noreply.github.com>
2021-10-26 20:20:55 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo apt update
 sudo apt upgrade -y
 
 echo "Installing packages"
-sudo apt-get install git zsh neovim curl terminator net-tools -y
+sudo apt-get install git zsh neovim curl terminator net-tools python3-pip -y
 
 echo "Installing gcc arm toolchain"
 wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2
