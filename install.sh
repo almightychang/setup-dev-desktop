f427abe04ac36180dc72413fc8305341fea626db
almightychang <65899329+almightychang@users.noreply.github.com>
2021-10-26 20:20:05 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ sudo ln -s /usr/lib/x86_64-linux-gnu/libtinfo.so.6 /usr/lib/x86_64-linux-gnu/lib
 
 echo "vim-plug for neovim"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-cp -r nvim/ ~/.config/nvim
+cp -r .config/nvim/ ~/.config/nvim
 
 echo "Installing pyenv..."
 git clone https://github.com/pyenv/pyenv.git
